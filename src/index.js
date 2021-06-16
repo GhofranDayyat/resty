9be3b032d6098868a7c56945b0c9b73c200174f6
ghofrandayyat <ghofrandayyat@gmail.com>
2021-06-16 23:56:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter,HashRouter} from 'react-router-dom'
+import Home from './Home'
+import History from './History'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 , document.getElementById('root1'));
